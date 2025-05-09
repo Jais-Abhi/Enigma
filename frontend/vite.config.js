4c3f95cd2e6ed96935f82dbf5779d568ab11e18f
Abhi <abhijaiswal7479@gmail.com>
2025-05-09 17:54:05 +0530

    deployement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Enigma",
+  base: "/",
   plugins: [react(), tailwind()],
 });
