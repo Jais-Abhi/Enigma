85dbc891b889ed4259a10ff102bec85675b4c2e4
Abhi <abhijaiswal7479@gmail.com>
2025-05-09 22:47:56 +0530

    header style added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         menuOpen ? "pb-4" : ""
       }`}
     >
-      <div className="flex flex-col md:flex-row justify-between items-center py-2 px-4">
+      <div className="flex flex-col md:flex-row justify-between items-center px-4">
         <div className="flex justify-between items-center w-full md:w-auto">
           <Link to="/" className="flex items-center gap-2">
             <img src="/enigma.png" alt="logo" className="h-16 md:h-20" />
@@ -35,32 +35,32 @@ const Header = () => {
         </div>
 
         <ul
-          className={`${
+          className={` font-[600] pl-10  box-border ${
             menuOpen ? "flex" : "hidden"
           } flex-col md:flex md:flex-row items-center gap-5 md:gap-8 text-lg md:text-base mt-3 md:mt-0`}
         >
-          <li className="hover:text-blue-600">
-            <NavLink to="/" className="flex items-center gap-1 pl-10">
+          <li>
+            <NavLink to="/" className={({ isActive }) => `flex items-center p-1 border-3 rounded-xl border-transparent ${isActive ? "text-blue-700" : "hover:text-blue-800 hover:border-blue-800 hover:transition-all hover:duration-700"}`}>
               <IoMdHome size="1.3rem" /> Home
             </NavLink>
           </li>
-          <li className="hover:text-blue-600">
-            <NavLink to="/events" className="flex items-center gap-1">
+          <li>
+            <NavLink to="/events" className={({ isActive }) => `flex items-center p-1 border-3 rounded-xl border-transparent ${isActive ? "text-blue-700" : "hover:text-blue-800 hover:border-blue-800 hover:transition-all hover:duration-700"}`}>
               <MdEventNote size="1.3rem" /> Events
             </NavLink>
           </li>
-          <li className="hover:text-blue-600">
-            <NavLink to="/about" className="flex items-center gap-1">
+          <li>
+            <NavLink to="/about" className={({ isActive }) => `flex items-center p-1 border-3 rounded-xl border-transparent ${isActive ? "text-blue-700" : "hover:text-blue-800 hover:border-blue-800 hover:transition-all hover:duration-700"}`}>
               <IoInformationCircle size="1.3rem" /> AboutUs
             </NavLink>
           </li>
-          <li className="hover:text-blue-600">
-            <NavLink to="/members" className="flex items-center gap-1">
+          <li>
+            <NavLink to="/members" className={({ isActive }) => `flex items-center p-1 border-3 rounded-xl border-transparent ${isActive ? "text-blue-700" : "hover:text-blue-800 hover:border-blue-800 hover:transition-all hover:duration-700"}`}>
               <FaPeopleGroup size="1.3rem" /> Members
             </NavLink>
           </li>
-          <li className="hover:text-blue-600">
-            <NavLink to="/contact" className="flex items-center gap-1">
+          <li>
+            <NavLink to="/contact" className={({ isActive }) => `flex items-center p-1 border-3 rounded-xl border-transparent ${isActive ? "text-blue-700" : "hover:text-blue-800 hover:border-blue-800 hover:transition-all hover:duration-700"}`}>
               <RiContactsBookFill size="1.3rem" /> ContactUs
             </NavLink>
           </li>
