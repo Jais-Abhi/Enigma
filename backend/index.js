1fb755e67262336b8e9cd3b12e1ce6ccf4a72286
adarshkumar76 <adarsh@kali>
2025-06-30 00:17:24 +0530

    modification for prod.

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,10 @@ import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-
 import authRouter from "./routes/auth/auth-route.js";
 import eventRouter from "./routes/event/event-route.js";
 import sliderRouter from "./routes/slider/slider-route.js";
 import memberRouter from "./routes/member/member-routes.js";
-
 dotenv.config();
 
 const DBURL = process.env.DB_URL;
@@ -27,7 +23,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://enigma-9a9i.onrender.com", // Change this to your frontend deployed URL if needed
+      "https://enigma-9a9i.onrender.com",
     ],
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
     allowedHeaders: [
@@ -48,14 +44,4 @@ app.use("/api/auth", authRouter);
 app.use("/api/event", eventRouter);
 app.use("/api/slider", sliderRouter);
 app.use("/api/member", memberRouter);
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-});
-
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
