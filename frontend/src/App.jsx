856c7a038f12d78d6dd470a35a2ede1932966bbf
Aditee Ray <royaditee259@gmail.com>
2025-08-11 09:47:49 +0530

    readme m developer component updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,15 @@ import "./App.css";
 import Body from "./Body";
 import store from "./store/store";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "./context/ThemeContext"; // ✅ new import
 
 function App() {
   return (
-    <>
-      <Provider store={store}>
-        {/* <Body /> */}
-      </Provider>
-    </>
+    <Provider store={store}>
+      <ThemeProvider>
+        <Body />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
